920d2c0354268966355c74df88b712fce7bc9839
sergiustaicu <32640040+sergiustaicu@users.noreply.github.com>
2018-03-25 19:46:29 +0200

    123

--- home.php
+++ home.php
@@ -6,8 +6,21 @@
 </head>
 <body>
 <div class="body">
-
-
+	<div class="header">
+		<div class="logo">
+			<img src="img\hooghoudt-logo.png" alt="Hooghoudt" height="74.4" width="280">
+		</div>
+		<div class="menubar">
+		</div>
+	</div>
+	<div class="mainpage">
+		<div class="comercial">
+		</div>
+		<div class="photos">
+		</div>
+	</div>
+	<div class="footer">
+	</div>
 </div>
 </body>
 </html>
