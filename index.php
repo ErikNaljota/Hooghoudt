b9940ab9fb5c57941c47a393a5697566a886b5e8
Erik_Naljota <erik.naljota@student.stenden.com>
2018-03-25 20:16:07 +0200

    123

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -23,6 +24,5 @@
           </form>
 
 		</div>
-
-
+  </div>
 </html>
---
+++ products.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hooghoudt - products</title>
+
+  <link rel="stylesheet" type="text/css" href="css/mainCSS.css">
+<head>
+
+<body>
+<div class="body">
+  <!-- Header Start -->
+  <div class="header">
+		<div class="logo">
+			<a href="home.php"><img src="img\hooghoudt-logo.png" alt="Hooghoudt" height="74.4" width="280"></a>
+		</div>
+		<div class="menubar">
+			<div class="home">
+			</div>
+			<div class="products">
+			</div>
+			<div class="since">
+			</div>
+			<div class="about">
+			</div>
+			<div class="receipe">
+			</div>
+			<div class="contact">
+			</div>
+		</div>
+	</div>
+  <!-- Header end -->
+
+  <!-- Content start -->
+  <div class="productsContent">
+    
+  </div>
+  <!-- Content end -->
+
+
+</div> <!-- body div end -->
+</body>
+</html>
