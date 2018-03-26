81957f493a4d9923e86b219e20367d6588a6a02b
sergiustaicu <32640040+sergiustaicu@users.noreply.github.com>
2018-03-27 00:56:25 +0200

    123

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -10,19 +9,20 @@
 	<body>
 
 	<div id="inputs_login">
-		<div class="logo">
-			<img src="img/hooghoudt-logo.png" alt="Hooghoudtp Logo">
+		<div class="logoLogin">
+			<img src="img/hooghoudt-logo.png" alt="Hooghoudtp Logo" style="width: 100%; text-align:center;">
 		</div>
 		<div class="fields">
-					<p><div class="fieldsText"><h1>Are you over 21?</h1></div></p>
+					<p><div class="fieldsText"><h1 style="font-size:25pt; margin-top: -30px;">Are you over 21?</h1></div></p>
 
           <form action="home.php" method="post" name="yes">
 					       <p><input class="fieldsText button" style=" margin-top: 10px; float: left;" type="submit" name="yes" value="Yes"></p>
           </form>
-          <form action="close.php" method="post">
+          <form action="404.php" method="post">
             <p><input class="fieldsText button" style=" margin-top: 10px; float: left;" type="submit" name="no" value="No"></p>
           </form>
 
 		</div>
   </div>
+</body>
 </html>
