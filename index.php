5bfed7ef2da4bdc46c408ea5ac51f0c51eb90d86
Erik_Naljota <erik.naljota@student.stenden.com>
2018-03-26 21:35:20 +0200

    123

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 	<body>
 
 	<div id="inputs_login">
-		<div class="logo">
-			<img src="img/hooghoudt-logo.png" alt="Hooghoudtp Logo">
+		<div class="logoLogin">
+			<img src="img/hooghoudt-logo.png" alt="Hooghoudtp Logo" style="width: 100%; text-align:center;">
 		</div>
 		<div class="fields">
-					<p><div class="fieldsText"><h1>Are you over 21?</h1></div></p>
+					<p><div class="fieldsText"><h1 style="font-size:25pt; margin-top: -30px;">Are you over 21?</h1></div></p>
 
           <form action="home.php" method="post" name="yes">
 					       <p><input class="fieldsText button" style=" margin-top: 10px; float: left;" type="submit" name="yes" value="Yes"></p>
